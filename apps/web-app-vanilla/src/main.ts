a84816ae985da19e0e7251af1d9154bd57dbed78
Omar <38073753+lordsayur@users.noreply.github.com>
2024-02-07 17:29:43 +0000

    ✨ implement basic drawing logic

--- apps/web-app-vanilla/src/main.ts
+++ apps/web-app-vanilla/src/main.ts
@@ -1,11 +1,16 @@
-import { Point } from "@virtual-world/core";
+import { Graph, Point, Segment } from "@virtual-world/core";
 
 import "./style.css";
 
 const ctx = createCanvasContext();
 
-const point = new Point(50, 50);
-point.draw(ctx);
+const point1 = new Point(50, 50);
+const point2 = new Point(350, 150);
+
+const segment1 = new Segment(point1, point2);
+
+const graph = new Graph([point1, point2], [segment1]);
+graph.draw(ctx);
 
 function createCanvasContext(): CanvasRenderingContext2D {
   const canvas = document.getElementById("virtual-world") as HTMLCanvasElement;
--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,3 +1,17 @@
+export class Graph {
+  constructor(private points: Point[], private segments: Segment[]) {}
+
+  draw(ctx: CanvasRenderingContext2D) {
+    for (const segment of this.segments) {
+      segment.draw(ctx);
+    }
+
+    for (const point of this.points) {
+      point.draw(ctx);
+    }
+  }
+}
+
 export class Point {
   x: number;
   y: number;
@@ -17,5 +31,22 @@ export class Point {
     ctx.fillStyle = color;
     ctx.arc(this.x, this.y, rad, 0, Math.PI * 2);
     ctx.fill();
+  }
+}
+
+export class Segment {
+  constructor(private point1: Point, private point2: Point) {}
+
+  draw(
+    ctx: CanvasRenderingContext2D,
+    width: number = 2,
+    color: string = "black"
+  ) {
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.strokeStyle = color;
+    ctx.moveTo(this.point1.x, this.point1.y);
+    ctx.lineTo(this.point2.x, this.point2.y);
+    ctx.stroke();
   }
 }
